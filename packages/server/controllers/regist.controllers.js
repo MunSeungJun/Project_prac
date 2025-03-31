b35fa979a884517a0d143e5343772c11238af10f
MunSeungJun <sbs2469@naver.com>
2025-03-31 12:33:35 +0900

    post 요청 처리
    1. 이미 존재하는 user 확인
    
    2. 없으면 create

--- packages/server/controllers/regist.controllers.js
+++ packages/server/controllers/regist.controllers.js
@@ -1,8 +1,25 @@
-const regist = (req, res) => {
-    res.status(200).json({
-        message:'success',
-        data: req.body
-    })
-}
+import User from "../models/user.js";
 
-export default regist
+const regist = async (req, res) => {
+    try {
+        const { username, passwd, phone, email } = req.body;
+    const exisUser = await User.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (exisUser) {
+      return res.status(400).json({
+        message: "존재하는 사용자 입니다",
+      });
+    }
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(200).json({
+      message: "success",
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default regist;
