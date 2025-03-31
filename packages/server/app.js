66ce70e7a90410b5f36038f66ee2a0a805a07aa0
MunSeungJun <sbs2469@naver.com>
2025-03-31 11:02:20 +0900

    미들웨어 추가
    1. express.json
    
    2. express.urlencoded

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,7 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config();
 import userRouter from './routes/user.js'
 import indexRouter from './routes/index.js'
 import connect from './schema/index.js'
@@ -9,9 +7,11 @@ import connect from './schema/index.js'
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.use('/', indexRouter)
-app.use('/api', userRouter)
-connect(process.env.DB_URI);
+app.use('/users', userRouter)
+
+connect();
 
 export default app;
