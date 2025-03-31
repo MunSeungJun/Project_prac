3911cc0d78a727ccb26e64fadc1b7c1bdb40490a
MunSeungJun <sbs2469@naver.com>
2025-03-31 10:10:59 +0900

    mongoDB 연결설정

--- packages/server/schema/index.js
+++ packages/server/schema/index.js
@@ -1,12 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-async function connect(uri) {
-    try{
-        await mongoose.connect(uri);
-        console.log('Connect success!!')
-    } catch (e) {
-        console.log(e)
-    }
+async function connect() {
+  if (process.env.NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
+  mongoose
+    .connect(process.env.DB_URI)
+    .then(() => console.log("mongoDB is connected!"))
+    .catch((err) => console.log(err));
 }
+mongoose.connection.on("error", () => {
+  console.log("mongoDB connect is failed");
+});
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB is disconnected");
+  connect();
+});
 
-export default connect
+export default connect;
