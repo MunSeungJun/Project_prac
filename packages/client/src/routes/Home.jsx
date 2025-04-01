83adef714fe40aef5ddf272ddf5f549d70364df3
MunSeungJun <sbs2469@naver.com>
2025-04-01 17:31:32 +0900

    home layout | slider component 장착(banners -> redux store변경 예정)

--- packages/client/src/routes/Home.jsx
+++ packages/client/src/routes/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Slider from '../components/Slider';
 
 const Home = () => {
+  const banners = [
+    {
+      uri: 'https://shop-phinf.pstatic.net/20250401_188/1743464323414MteEj_JPEG/EB89B4EC87BCED9591_ED9988ECB59CEC8381EB8BA8_MO_1152x180282.jpg?type=a2304_jpg',
+    },
+    {
+      uri: "https://shop-phinf.pstatic.net/20250326_129/1742955390534BsHng_PNG/EB89B4EC87BCED9591_ECB59CEC8381EB8BA8MO_1152x180.png?type=a2304_jpg"
+    },
+    {
+      uri: "https://shop-phinf.pstatic.net/20250327_45/1743061593987NeE0J_PNG/EB89B4EC87BCED9591_ECB59CEC8381EB8BA8m_1152x180.png?type=a2304_jpg"
+    }
+  ];
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <main>
+        <Container>
+          <Slider indicators={false} datas={banners} />
+        </Container>
+      </main>
+    </>
+  );
+};
 
-export default Home
+export default Home;
