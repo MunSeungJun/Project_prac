dd4e01d5cee5459189be607b6ec38ae2548810d9
MunSeungJun <sbs2469@naver.com>
2025-04-01 11:44:42 +0900

    import index.css

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter} from 'react-router-dom'
-import './styles/index.css'
 import App from './App.jsx'
+import './styles/index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
