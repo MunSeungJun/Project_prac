ec4a7cce65b2efc342c487e157cc0965d23971fe
MunSeungJun <sbs2469@naver.com>
2025-04-01 14:18:36 +0900

    update root route

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Index from './routes/Index';
 import Home from './routes/Home';
 import SignUp from './routes/SignUp';
 
@@ -7,8 +8,10 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Index />}>
+          <Route index element={<Home/>}  />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
       </Routes>
     </>
   );
