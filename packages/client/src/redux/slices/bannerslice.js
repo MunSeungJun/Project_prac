56cb162f76dce8eaca263b9d4d572dac4c301e19
MunSeungJun <sbs2469@naver.com>
2025-04-02 14:19:07 +0900

    bannerslice | initialState 더미 데이터 추가

--- packages/client/src/redux/slices/bannerslice.js
+++ packages/client/src/redux/slices/bannerslice.js
@@ -1,16 +1,26 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = { banners:[] }
+const initialState = {
+  banners: [
+    {
+      id: 1,
+      uri: 'https://shop-phinf.pstatic.net/20250401_188/1743464323414MteEj_JPEG/EB89B4EC87BCED9591_ED9988ECB59CEC8381EB8BA8_MO_1152x180282.jpg?type=a2304_jpg',
+    },
+    {
+      id: 2,
+      uri: 'https://shop-phinf.pstatic.net/20250326_129/1742955390534BsHng_PNG/EB89B4EC87BCED9591_ECB59CEC8381EB8BA8MO_1152x180.png?type=a2304_jpg',
+    },
+    {
+      id: 3,
+      uri: 'https://shop-phinf.pstatic.net/20250327_45/1743061593987NeE0J_PNG/EB89B4EC87BCED9591_ECB59CEC8381EB8BA8m_1152x180.png?type=a2304_jpg',
+    },
+  ],
+};
 
 const bannerslice = createSlice({
-    name:"banner",
-    initialState: initialState,
-    reducers:[
-        {
+  name: 'banner',
+  initialState: initialState,
+  reducers: [{}],
+});
 
-        }
-    ]
-})
-
-export const {} = bannerslice.actions
-export default bannerslice.reducer
+export default bannerslice.reducer;
