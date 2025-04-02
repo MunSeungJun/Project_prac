be96af34686ab54d08c2226d135b9abcfd2cad91
MunSeungJun <sbs2469@naver.com>
2025-04-02 17:23:47 +0900

    home Ui 임시

--- packages/client/src/routes/Home.jsx
+++ packages/client/src/routes/Home.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container,Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+import { useSelector} from 'react-redux'
 import Slider from '../components/Slider';
+import '../styles/Home.css'
 
 const Home = () => {
-  const banners = [
-    {
-      uri: 'https://shop-phinf.pstatic.net/20250401_188/1743464323414MteEj_JPEG/EB89B4EC87BCED9591_ED9988ECB59CEC8381EB8BA8_MO_1152x180282.jpg?type=a2304_jpg',
-    },
-    {
-      uri: "https://shop-phinf.pstatic.net/20250326_129/1742955390534BsHng_PNG/EB89B4EC87BCED9591_ECB59CEC8381EB8BA8MO_1152x180.png?type=a2304_jpg"
-    },
-    {
-      uri: "https://shop-phinf.pstatic.net/20250327_45/1743061593987NeE0J_PNG/EB89B4EC87BCED9591_ECB59CEC8381EB8BA8m_1152x180.png?type=a2304_jpg"
-    }
-  ];
+  const banners = useSelector((state) => state.banner.banners)
+
   return (
     <>
       <main>
         <Container>
-          <Slider indicators={false} datas={banners} />
+            <Row>
+              <div className='category col-4'>
+                <ul>
+                  <li><Link>Woman's Fashion</Link></li>
+                  <li><Link>Mens's Fashion</Link></li>
+                  <li><Link>Electronics</Link></li>
+                  <li><Link>Home & Lifestyle</Link></li>
+                  <li><Link>Medicine</Link></li>
+                  <li><Link>Sports & Outdoor</Link></li>
+                  <li><Link>Baby's & Toys</Link></li>
+                  <li><Link>Groceries & Pets</Link></li>
+                  <li><Link>Health & Beauty</Link></li>
+                </ul>
+              </div>
+              <div className='col-8'>
+                <Slider  indicators={false} datas={banners}  />
+              </div>
+            </Row>
         </Container>
       </main>
     </>
