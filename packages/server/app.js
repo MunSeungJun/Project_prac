2021ba1944fa62a86bfe9d44149f0651b35a13d3
MunSeungJun <sbs2469@naver.com>
2025-04-02 14:17:25 +0900

    import morgan

--- packages/server/app.js
+++ packages/server/app.js
@@ -3,15 +3,17 @@ import cors from 'cors'
 import userRouter from './routes/user.js'
 import indexRouter from './routes/index.js'
 import connect from './schema/index.js'
+import logger from 'morgan'
 
 const app = express()
+app.use(logger('dev'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+connect();
 app.use('/', indexRouter)
 app.use('/users', userRouter)
 
-connect();
 
 export default app;
