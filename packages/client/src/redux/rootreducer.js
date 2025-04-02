7e960c59a5a2ce14aa9249f098169d8fa71082c6
MunSeungJun <sbs2469@naver.com>
2025-04-02 14:17:51 +0900

    rootreducer 삭제

--- packages/client/src/redux/rootreducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import bannerreducer from './slices/bannerslice'
-
-const rootreducer = combineReducers({
-    banner: bannerreducer 
-})
-
-export default rootreducer
