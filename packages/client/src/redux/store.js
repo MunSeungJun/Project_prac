c5894e737c1a84908ebedbbc77d3047cbd987dd4
MunSeungJun <sbs2469@naver.com>
2025-04-03 14:06:50 +0900

    authreducer 생성 후 store에 등록

--- packages/client/src/redux/store.js
+++ packages/client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bannerreducer from './slices/bannerslice';
+import authreducer  from './slices/authslice'
 
 
 const store = configureStore({
   reducer: {
     banner : bannerreducer,
+    auth : authreducer
   }
 });
 
