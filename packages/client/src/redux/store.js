d458ba7530841f137c330bccf36459f5c80e8418
MunSeungJun <sbs2469@naver.com>
2025-04-03 09:44:04 +0900

    signupReducer, signupSlice 삭제

--- packages/client/src/redux/store.js
+++ packages/client/src/redux/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bannerreducer from './slices/bannerslice';
-import signupreducer from './slices/signupslice'
+
 
 const store = configureStore({
   reducer: {
     banner : bannerreducer,
-    signup : signupreducer
   }
 });
 
