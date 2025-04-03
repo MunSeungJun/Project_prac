8a9e1e2fb154fff13abdb324dfac83b2289b3544
MunSeungJun <sbs2469@naver.com>
2025-04-03 14:19:03 +0900

    route 추가
    1. index route에 자식 route signin 추가

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Index from './routes/Index';
 import Home from './routes/Home';
 import SignUp from './routes/SignUp';
+import SignIn from './routes/SignIn';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Index />}>
           <Route index element={<Home/>}  />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
         </Route>
       </Routes>
     </>
