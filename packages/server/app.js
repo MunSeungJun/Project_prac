ddb333876065e673d1706dc55b22c6dfd33ac2bf
MunSeungJun <sbs2469@naver.com>
2025-04-04 12:50:10 +0900

    미들웨어 장착 및 옵션 추가
    1. cookie-parser 장착
    
    2. cors에 credentials 옵션 추가

--- packages/server/app.js
+++ packages/server/app.js
@@ -4,12 +4,17 @@ import userRouter from './routes/user.js'
 import indexRouter from './routes/index.js'
 import connect from './schema/index.js'
 import logger from 'morgan'
+import cookieParser from "cookie-parser";
 
 const app = express()
 app.use(logger('dev'))
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
 
 connect();
 app.use('/', indexRouter)
