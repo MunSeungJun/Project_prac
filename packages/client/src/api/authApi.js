3b0a207d9f1953a96dc65589a04765f28ab30fb0
MunSeungJun <sbs2469@naver.com>
2025-04-04 14:03:49 +0900

    create authApi
    1. 회원가입, 로그인, 로그아웃, 로그인상태유지 api 모듈화

---
+++ packages/client/src/api/authApi.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { login, logout } from '../redux/slices/authslice';
+
+export const setUser = async formData => {
+  try {
+    const response = axios.post('http://localhost:3000/users/regist', formData);
+    console.log(await response);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const setLogin = async (formData, dispatch) => {
+  try {
+    axios.post('http://localhost:3000/users/login', formData, { withCredentials: true });
+    dispatch(login());
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const setLogout = async (dispatch) => {
+    try {
+        await axios.get('http://localhost:3000/users/logout', {
+          withCredentials: true,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+      dispatch(logout());
+}
+
+export const setAuth = async () => {
+  const response = await axios.get('http://localhost:3000/users/check', {
+    withCredentials: true,
+  });
+  return response.data.loggedIn;
+};
