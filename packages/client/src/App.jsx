a76119cb11eaafec5a173e3d185497bbf3eed565
MunSeungJun <sbs2469@naver.com>
2025-04-04 14:13:46 +0900

    로그인 상태 유지 코드 추가
    1. 화면 새로고침시 checkAuth 액션 생성함수 호출로 로그인 상태 변수 변경

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { checkAuth } from '../redux/slices/authslice';
+import { useDispatch } from 'react-redux';
 import Index from './routes/Index';
 import Home from './routes/Home';
 import SignUp from './routes/SignUp';
 import SignIn from './routes/SignIn';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Index />}>
-          <Route index element={<Home/>}  />
+          <Route index element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
         </Route>
