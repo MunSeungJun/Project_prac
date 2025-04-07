a918532138915183f40453e5dfebfc9077537eb2
MunSeungJun <sbs2469@naver.com>
2025-04-07 18:01:04 +0900

    feature : HomeSaleProducts 컴포넌트 추가(나머지 섹션 모듈화 필요)

--- packages/client/src/features/home/pages/Home.jsx
+++ packages/client/src/features/home/pages/Home.jsx
@@ -1,37 +1,55 @@
 import React from 'react';
-import { Container,Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
-import { useSelector} from 'react-redux'
+import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Slider from '../components/Slider';
-import '../../../styles/home.css'
+import HomeSaleProducts from '../components/HomeSaleProducts';
+import '../../../styles/home.css';
 
 const Home = () => {
-  const banners = useSelector((state) => state.banner.banners)
+  const banners = useSelector(state => state.banner.banners);
 
   return (
     <>
-      <main>
-        <Container>
-            <Row>
-              <div className='category col-4'>
-                <ul>
-                  <li><Link>Woman's Fashion</Link></li>
-                  <li><Link>Mens's Fashion</Link></li>
-                  <li><Link>Electronics</Link></li>
-                  <li><Link>Home & Lifestyle</Link></li>
-                  <li><Link>Medicine</Link></li>
-                  <li><Link>Sports & Outdoor</Link></li>
-                  <li><Link>Baby's & Toys</Link></li>
-                  <li><Link>Groceries & Pets</Link></li>
-                  <li><Link>Health & Beauty</Link></li>
-                </ul>
-              </div>
-              <div className='col-8'>
-                <Slider  indicators={false} datas={banners}  />
-              </div>
-            </Row>
-        </Container>
-      </main>
+      <section>
+        <Row>
+          <div className="category col-4">
+            <ul>
+              <li>
+                <Link>Woman's Fashion</Link>
+              </li>
+              <li>
+                <Link>Mens's Fashion</Link>
+              </li>
+              <li>
+                <Link>Electronics</Link>
+              </li>
+              <li>
+                <Link>Home & Lifestyle</Link>
+              </li>
+              <li>
+                <Link>Medicine</Link>
+              </li>
+              <li>
+                <Link>Sports & Outdoor</Link>
+              </li>
+              <li>
+                <Link>Baby's & Toys</Link>
+              </li>
+              <li>
+                <Link>Groceries & Pets</Link>
+              </li>
+              <li>
+                <Link>Health & Beauty</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-8">
+            <Slider indicators={false} datas={banners} />
+          </div>
+        </Row>
+      </section>
+      <HomeSaleProducts/>
     </>
   );
 };
