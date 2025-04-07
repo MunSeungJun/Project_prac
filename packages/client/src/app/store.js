708dd5c439e27b4c0c7bfd275b37cfc459fe0e62
MunSeungJun <sbs2469@naver.com>
2025-04-07 17:58:44 +0900

    feature :  store에 homeSaleProducts리듀서 등록

--- packages/client/src/app/store.js
+++ packages/client/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import bannerreducer from '../features/home/homeslice';
-import authreducer  from '../features/auth/authslice'
+import bannerreducer from '../features/home/slices/bannerslice';
+import homeSaleProductsreducer from '../features/home/slices/homeSaleProductsslice'
+import authreducer  from '../features/auth/slices/authslice'
 
 
 const store = configureStore({
   reducer: {
     banner : bannerreducer,
+    homeSaleProducts: homeSaleProductsreducer,
     auth : authreducer
   }
 });
